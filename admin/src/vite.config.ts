0b14313675aab7ea20baac3457771137acbf7228
tim177 <timaraw18@gmail.com>
2025-04-11 11:00:08 +0530

    host defined

--- admin/src/vite.config.ts
+++ admin/src/vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000", // your local backend
-        changeOrigin: true,
-      },
-    },
+    host: "0.0.0.0",
+    port: 5173, // or any other port you want Render to use
   },
 });
